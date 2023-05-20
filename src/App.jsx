27ab1040021c723db71846606242d5d97c29952e
Unreachedx <oliphant.p@web.de>
2023-05-20 05:36:23 +0200

    Session 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Header from './components/Header/Header'
+import { BrowserRouter } from 'react-router-dom'
+import ThemeContextProvider from './contexts/ThemeContext'
 
 function App() {
 
   return (
-    <>
-      <div>
-
-      </div>
-    </>
+      <ThemeContextProvider>
+        <BrowserRouter>
+          <Header />    
+        </BrowserRouter>
+      </ThemeContextProvider>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import './Header.css'
+import React, { useContext } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 function Header() {
-  return (
-    <header className='header-container'>
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
 
+  const handleTheme = () => {
+    const newDarkMode = !darkMode;
+    setDarkMode(newDarkMode);
+    localStorage.setItem('darkMode', newDarkMode);
+  };
+
+  return (
+    <header className={`header-container ${!darkMode && "header-light"}`}>
+      <Link to="/" className="logo">
+        CineTrail
+      </Link>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Movies"
+          className="search-input"
+        />
+      </div>
+      <div className="header-buttons-container">
+        {darkMode ? (
+          <div className="theme-button-container">
+            <MdOutlineLightMode onClick={handleTheme} className="theme-icon" />
+            <MdOutlineDarkMode className="theme-icon theme-icon-active" />
+          </div>
+        ) : (
+          <div className="theme-button-container">
+            <MdOutlineLightMode className="theme-icon theme-icon-active"
+            />
+            <MdOutlineDarkMode onClick={handleTheme} className="theme-icon" />
+          </div>
+        )}
+        <button className="create-account-btn">Create an account</button>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
