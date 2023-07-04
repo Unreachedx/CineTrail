a936e99831109bbf0465eacb9cabc0ab1b2bdf1e
Unreachedx <oliphant.p@web.de>
2023-07-04 16:23:15 +0200

    new

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ Genres.css'
+import './Genres.css'
 
 export default function Genres({ movieGenres, baseUrl, apiKey,component }) {
 
